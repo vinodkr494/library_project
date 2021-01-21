c4406fe13e96547fdc3403ac0a56503cac96156d
vinod kumar <vinod.kr494@gmail.com>
2021-01-21 17:43:22 +0530

    adding requirement.txt and book list views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book
 
-# Create your views here.
+
+class BookListView(ListView):
+    """Render book in list view"""
+    model = Book
+    template_name = 'book_list.html'
